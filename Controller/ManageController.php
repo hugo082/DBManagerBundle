c2869c07f0fb49026afd780d00f78303a0195466
hugo_082 <hugo.fouquet@epita.fr>
2017-05-05 19:27:58 +0200

    Support custom and default action

--- Controller/ManageController.php
+++ Controller/ManageController.php
@@ -36,13 +36,29 @@ class ManageController extends Controller
         ));
     }
 
+    public function processAction(Request$request, $method, $name, $id) {
+        /** @var $checker ActionManager */
+        $actionManager = $this->get('fqt.dbcm.manager.action');
+        $execution = $actionManager->customAction($request, $method, $name, $id);
+
+        if ($execution["data"] != null && key_exists('flash', $execution["data"])) {
+            foreach ($execution["data"]["flash"] as $flash)
+                $this->addFlash($flash["type"], $flash["message"]);
+        }
+
+        return $this->redirectToRoute('db.manager.list', array('name' => $name));
+    }
+
     public function listAction(Request $request, $name)
     {
         /** @var $checker ActionManager */
         $actionManager = $this->get('fqt.dbcm.manager.action');
-        $execution = $actionManager->listAction($request, $name);
+        $execution = $actionManager->listAction($name);
 
-        $form = NULL;
+        var_dump($execution["entityInfo"]);
+        $execution["entityInfo"]["displayElements"] = $this->getDisplayPermissions($execution["entityInfo"], "null");
+
+        $form = null;
         if ($execution["entityInfo"]['displayElements'][Conf::DISP_ELEM_FORM]['full']) {
             $process = $actionManager->processAddForm($request, $execution["entityInfo"]);
             if ($process["success"])
@@ -108,5 +124,28 @@ class ManageController extends Controller
         else
             $this->addFlash('danger','Your entity could not be deleted');
         return $this->redirectToRoute('db.manager.list', array('name' => $name));
+    }
+
+
+    /**
+     * Compute current displayable elements for entity / user / action
+     * WARNING : don't check $eInfo['permissions'][$action]
+     * @param array $entity
+     * @param string $action
+     * @return array
+     */
+    private function getDisplayPermissions(array $entity, string $action) {
+        #$formAction = ($action == Conf::PERM_LIST) ? Conf::PERM_ADD : $action;
+        return array(
+            Conf::DISP_ELEM_FORM => array(
+                'full' => true, #$full = $this->settings[$action][Conf::DISP_ELEM_FORM] && $this->entityAccess($entity, $formAction),
+                Conf::DISP_ELEM_ADDLINK => true, #!$full and $entity['permissions'][$formAction] && $formAction != $action && $this->entityAccess($entity, $formAction)
+            ),
+            Conf::DISP_ELEM_LIST => array(
+                'full' => true, #$full = $this->settings[$action][Conf::DISP_ELEM_LIST] and $this->entityAccess($entity, Conf::PERM_LIST),
+                Conf::DISP_ELEM_EDITLINK => true, #$full and $entity['permissions'][Conf::PERM_EDIT] and $this->entityAccess($entity, Conf::PERM_EDIT),
+                Conf::DISP_ELEM_REMOVELINK => true, #$full and $entity['permissions'][Conf::PERM_REMOVE] and $this->entityAccess($entity, Conf::PERM_REMOVE)
+            )
+        );
     }
 }
